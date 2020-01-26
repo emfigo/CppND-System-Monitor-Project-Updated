2b6c8f97682121cc9d6252ded063d07668679a54
emfigo <emfigo@gmail.com>
2020-01-26 15:25:00 +0000

    🐤 Add first version of format class

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
 #include <string>
 
 #include "format.h"
 
 using std::string;
+using std::stringstream;
+using std::to_string;
+using std::setw;
+using std::setfill;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+  int hour, min;
+  stringstream strm;
+
+  hour = seconds / 3600;
+  seconds = seconds % 3600;
+  min = seconds / 60;
+  seconds = seconds % 60;
+
+  strm << setw(2) << setfill('0') << hour << ':';
+  strm << setw(2) << setfill('0') << min << ':';
+  strm << setw(2) << setfill('0') << seconds;
+
+  return strm.str();
+}
