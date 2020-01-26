a191a62f7fede9f89bce2f3f57e2be4cd7c12419
emfigo <emfigo@gmail.com>
2020-01-26 15:26:34 +0000

    🐦 Add first version of processor class

--- include/processor.h
+++ include/processor.h
@@ -1,12 +1,35 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+struct CPUTimes {
+  int user;
+  int nice;
+  int system;
+  int idle;
+  int iowait;
+  int irq;
+  int softirq;
+  int steal;
+  int guest;
+  int guest_nice;
+};
+
 class Processor {
  public:
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
  private:
+  int prevuser_ = 0;
+  int prevnice_ = 0;
+  int prevsystem_ = 0;
+  int previdle_ = 0;
+  int previowait_ = 0;
+  int previrq_ = 0;
+  int prevsoftirq_ = 0;
+  int prevsteal_ = 0;
+  int prevguest_ = 0;
+  int prevguest_nice_ = 0;
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,53 @@
+#include <string>
+#include <vector>
+
+#include "linux_parser.h"
 #include "processor.h"
 
+using std::string;
+using std::vector;
+using std::stoi;
+
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() { 
+  vector<string> utilization = LinuxParser::CpuUtilization();
+  CPUTimes times;
+  times.user = stoi(utilization[0]);
+  times.nice = stoi(utilization[1]);
+  times.system = stoi(utilization[2]);
+  times.idle = stoi(utilization[3]);
+  times.iowait = stoi(utilization[4]);
+  times.irq = stoi(utilization[5]);
+  times.softirq = stoi(utilization[6]);
+  times.steal = stoi(utilization[7]);
+  times.guest = stoi(utilization[8]);
+  times.guest_nice = stoi(utilization[9]);
+
+  int PrevIdle = previdle_ + previowait_;
+  int Idle = times.idle + times.iowait;
+
+  int PrevNonIdle = prevuser_ + prevnice_ + prevsystem_ + previrq_ + prevsoftirq_ + prevsteal_;
+  int NonIdle = times.user + times.nice + times.system + times.irq + times.softirq + times.steal;
+
+  int PrevTotal = PrevIdle + PrevNonIdle;
+  int Total = Idle + NonIdle;
+
+  // differentiate: actual value minus the previous one
+  int totald = Total - PrevTotal;
+  int idled = Idle - PrevIdle;
+
+  float CPU_Percentage = ((totald - idled) * 1.0 ) / (totald * 1.0);
+
+  prevuser_ = times.user;
+  prevnice_ = times.nice;
+  prevsystem_ = times.system;
+  previdle_ = times.idle;
+  previowait_ = times.iowait;
+  previrq_ = times.irq;
+  prevsoftirq_ = times.softirq;
+  prevsteal_ = times.steal;
+  prevguest_ = times.guest;
+  prevguest_nice_ = times.guest_nice;
+
+  return CPU_Percentage;
+}
